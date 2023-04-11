4aa878c424cc9da20e38a5063bdd2c9ad6cb9857
Symeon Melnychuk <m.simeon998@gmail.com>
2023-04-11 17:23:58 +0300

    5

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -8,7 +8,7 @@ const LOCALSTORAGE_KEY = "videoplayer-current-time";
 
 
 player.on('timeupdate', throttle ((data) => {
-localStorage.setItem(LOCALSTORAGE_KEY, data.seconds)}, 1000))
+localStorage.setItem(LOCALSTORAGE_KEY, data.seconds)}, 1000));
     
 
 const getSecond = localStorage.getItem(LOCALSTORAGE_KEY);
